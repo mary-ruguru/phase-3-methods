5ec312b464b16bf444dd86f61e44b36e21423115
Maryann Ruguru Njuguna <maryannruguru20@gmail.com>
2023-02-07 15:40:24 +0300

    completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
 # Your code here!
+
+def greet_programmer
+puts "Hello, programmer!"
+
+end
+
+def greet (name)
+  puts "Hello, #{name}!"
+end
+greet ("Naureen")
+
+def greet_with_default(name= "programmer")
+  puts "Hello, #{name}!"
+end
+
+greet_with_default("Naureen")
+greet_with_default
+
+def add(a , b)
+   a + b
+end
+
+def halve(num)
+  if num.class != Integer
+    return nil
+  end
+  num / 2
+end
+
+
+      
+         
